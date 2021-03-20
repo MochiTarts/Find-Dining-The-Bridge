2b3c1562dcbe74a2bddece026f44c534eec67f34
Jayden <j_aydentse@hotmail.com>
2021-03-19 20:29:46 -0400

    Created backend for subscriber profile

--- backend/utils/common.py
+++ backend/utils/common.py
@@ -2,3 +2,17 @@ from bson import ObjectId
 
 def isObjectId(oid):
     return ObjectId.is_valid(oid)
+
+def save_and_clean(model, updated_fields=None):
+    """
+    clean model and then save
+    :params-model: referenced model
+    :return: saved model
+    """
+    model.clean_fields()
+    model.clean()
+    if updated_fields:
+        model.save(update_fields=updated_fields)
+    else:
+        model.save()
+    return model
