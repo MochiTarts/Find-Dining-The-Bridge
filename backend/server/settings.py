ca01c67f503cf056105e28e8dea7f7e0f77178d2
Jayden <j_aydentse@hotmail.com>
2021-03-25 17:34:32 -0400

    Added static files

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -145,6 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Override default user model
 AUTH_USER_MODEL = 'sduser.SDUser'
