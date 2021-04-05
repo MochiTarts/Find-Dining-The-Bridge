cd4bda804f74cdd7202732c71e7a7dfbebbc25ac
MochiTarts <jenny100.yu@gmail.com>
2021-04-05 12:13:23 -0400

    Made base service for retrieving articles

---
+++ client-server/src/app/_services/article.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const AUTH_API = '/api';
+const ARTICLE_API = AUTH_API + '/'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArticleService {
+
+  constructor(private http: HttpClient) { }
+
+  getArticles(): Observable<any> {
+    const endpoint = ARTICLE_API;
+    return this.http.get(endpoint);
+  }
+
+}
