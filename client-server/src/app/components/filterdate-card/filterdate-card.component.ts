1364ba94284071149d67d36754dc4e902c5e1969
MochiTarts <jenny100.yu@gmail.com>
2021-04-08 22:33:44 -0400

    Finished most of news article page

--- client-server/src/app/components/filterdate-card/filterdate-card.component.ts
+++ client-server/src/app/components/filterdate-card/filterdate-card.component.ts
@@ -9,11 +9,8 @@ export class FilterdateCardComponent implements OnInit {
 
   @Input() months: string[];
   @Input() years: string[];
-  @Input() monthsLength: number;
-  @Input() yearsLength: number;
 
-  @Output() monthEvents: EventEmitter<any> = new EventEmitter<any>();
-  @Output() yearEvents: EventEmitter<any> = new EventEmitter<any>();
+  @Output() events: EventEmitter<any> = new EventEmitter<any>();
 
   monthCheckMap: boolean[] = new Array(length);
   yearCheckMap: boolean[] = new Array(length);
@@ -21,11 +18,11 @@ export class FilterdateCardComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    for (let i = 0; i < this.monthsLength; i++) {
+    for (let i = 0; i < 12; i++) {
       this.monthCheckMap.push(false);
     }
 
-    for (let i = 0; i < this.yearsLength; i++) {
+    for (let i = 0; i < 6; i++) {
       this.yearCheckMap.push(false);
     }
   }
@@ -38,12 +35,14 @@ export class FilterdateCardComponent implements OnInit {
         if (inputs[i].type == "checkbox") { 
             inputs[i].checked = true;
         }
-        if (inputs[i].name == "months") {
-          this.monthCheckMap.push(true);
-        }
-        if (inputs[i].name == "years") {
-          this.yearCheckMap.push(true);
-        }
+    }
+
+    for (let i = 0; i < 12; i++) {
+      this.monthCheckMap.push(true);
+    }
+
+    for (let i = 0; i < 6; i++) {
+      this.yearCheckMap.push(true);
     }
   }
 
@@ -55,18 +54,23 @@ export class FilterdateCardComponent implements OnInit {
         if (inputs[i].type == "checkbox") { 
             inputs[i].checked = false;
         }
-        if (inputs[i].name == "months") {
-          this.monthCheckMap.push(false);
-        }
-        if (inputs[i].name == "years") {
-          this.yearCheckMap.push(false);
-        }
+    }
+
+    for (let i = 0; i < 12; i++) {
+      this.monthCheckMap.push(false);
+    }
+
+    for (let i = 0; i < 6; i++) {
+      this.yearCheckMap.push(false);
     }
   }
 
   sendList() {
-    this.monthEvents.emit(this.monthCheckMap);
-    this.yearEvents.emit(this.yearCheckMap);
+    var checkMap = {
+      month: this.monthCheckMap,
+      year: this.yearCheckMap
+    }
+    this.events.emit(checkMap);
   }
 
 }
