5031c9d0c9ab63aae2ee2090e6bd2380ddf57cdd
mouisaac <mouisaac88801@gmail.com>
2021-04-12 15:33:48 -0400

    fixed settings on static root; modified signup backend to automatically create superuser for info@finddining.ca on registration; fixed sign up message display;

--- backend/sduser/backends.py
+++ backend/sduser/backends.py
@@ -101,6 +101,12 @@ def create_disable_user_and_send_verification_email(user, password, request):
     form = SDUserCreateForm(data=user)
 
     if form.is_valid():
+
+        # create Admin user if the email is info@finddining.ca
+        if user['email'] == "info@finddining.ca":
+            UserModel.objects.create_superuser(
+            username=user['username'], email=user['email'], password=user['password'], role=user['role'], )
+            return JsonResponse({'message': "An admin account has been created. You can now log in with the registered credentials."})
         user = UserModel.objects.create_user(
             username=user['username'], email=user['email'], password=user['password'], role=user['role'])
         user.is_active = False
