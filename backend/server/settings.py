09dc67dc8580863c68d6b6f7f39e2616690250b2
mouisaac <mouisaac88801@gmail.com>
2021-04-12 10:18:07 -0400

    updated csrf trusted origin;

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -206,8 +206,9 @@ CORS_ALLOW_METHODS = (
  )
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:4200",
-    "http://127.0.0.1:4200",
+    #"http://localhost:4200",
+    #"http://127.0.0.1:4200",
+    ".finddining.ca",
 ]
 
 #CSRF_COOKIE_NAME = 'XSRF-TOKEN'
