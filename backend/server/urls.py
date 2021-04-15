9d56fde2c165be45365e0af03cdef3173d6da96b
MochiTarts <jenny100.yu@gmail.com>
2021-04-15 09:31:23 -0400

    Removed auth_refresh_view swagger url

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -85,7 +85,6 @@ if settings.DEBUG:
         url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
         url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
         url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-        url(r'^redoc/#operation/POST%20/auth_refresh_create/$', sduser.backends.auth_refresh_view, name='schema-redoc'),
     ]
     urlpatterns.extend(doc_urls)
 
