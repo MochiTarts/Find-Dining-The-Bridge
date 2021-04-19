019fa54e1fcb06b9304b869c03d0bd7108dc8ec2
MochiTarts <jenny100.yu@gmail.com>
2021-04-19 13:38:26 -0400

    Made tests for restaurant

--- backend/restaurant/tests.py
+++ backend/restaurant/tests.py
@@ -3,7 +3,8 @@ from rest_framework.test import APIClient, force_authenticate
 from django.contrib.auth import get_user_model
 
 from .models import (
-    Restaurant
+    Restaurant,
+    PendingRestaurant
 )
 from .views import (
     AllRestaurantList,
@@ -22,13 +23,12 @@ User = get_user_model()
 class ApprovedRestaurantTestCases(TestCase):
     """ Tests for restaurants from the Restaurant collection """
 
-    @classmethod
-    def setUpClass(cls):
-        cls.maxDiff = None
-        cls.client = APIClient()
-        cls.restaurant_1 = Restaurant.objects.create(name="restaurant 1", email="1@mail.com")
-        cls.restaurant_2 = Restaurant.objects.create(name="restaurant 2", email="2@mail.com")
-        cls.restaurant_3 = Restaurant.objects.create(name="restaurant 3", email="3@mail.com")
+    def setUp(self):
+        self.maxDiff = None
+        self.client = APIClient()
+        self.restaurant_1 = Restaurant.objects.create(name="restaurant 1", email="1@mail.com")
+        self.restaurant_2 = Restaurant.objects.create(name="restaurant 2", email="2@mail.com")
+        self.restaurant_3 = Restaurant.objects.create(name="restaurant 3", email="3@mail.com")
 
     def test_get_all_restaurants(self):
         """ Test if all approved restaurants are retrieved """
@@ -43,8 +43,53 @@ class ApprovedRestaurantTestCases(TestCase):
             rest.payment_methods = ['/']
             rest.offer_options = ['']
             rest.save()
-            
+
         expected = {
             "Restaurants": models_to_json(restaurants)
         }
         self.assertDictEqual(json.loads(response.content), expected)
+
+    def test_get_approved_restaurant(self):
+        """ Test if approved restaurant is retrieved by _id """
+        _id = Restaurant.objects.get(email="1@mail.com")._id
+        response = self.client.get('/api/restaurant/approved/'+str(_id)+'/')
+
+        restaurant = Restaurant.objects.get(email="1@mail.com")
+        restaurant.restaurant_image_url = ['/']
+        restaurant.payment_methods = ['/']
+        restaurant.offer_options = ['']
+        restaurant.save()
+
+        expected = model_to_json(restaurant)
+        self.assertDictEqual(json.loads(response.content), expected)
+
+
+class DraftRestaurantTestCases(TestCase):
+    """ Tests for pending restaurants marked as drafts """
+
+    def setUp(self):
+        self.maxDiff = None
+        self.client = APIClient()
+        self.ro = User.objects.create(username="TestOwner", role="RO")
+        self.client.force_authenticate(user=self.ro)
+        self.restaurant_draft = PendingRestaurant.objects.create(name="test draft", email="draft@mail.com")
+
+    def test_insert_restaurant_draft(self):
+        """ Tests to see if restaurant draft is inserted correctly """
+        restaurant_draft = {
+            "name": "Test Restaurant",
+            "address": "300 Borough Dr",
+            "postalCode": "M1C 3A4",
+            "email": "bob@mail.com",
+            "owner_first_name": ["Bob"],
+            "owner_last_name": ["Smith"]
+        }
+        response = self.client.post('/api/restaurant/draft/', restaurant_draft, format='json')
+        self.assertTrue(PendingRestaurant.objects.filter(
+            name="Test Restaurant",
+            address="300 Borough Dr",
+            postalCode="M1C 3A4",
+            email="bob@mail.com",
+            owner_first_name=["Bob"],
+            owner_last_name=["Smith"],
+            status="In_Progress").exists())
