2a2a9b91df8d364bfe2f3efa2c975e49f01350f6
mouisaac <mouisaac88801@gmail.com>
2021-04-21 10:29:43 -0400

    updated email templates and fixed small issues;

--- backend/restaurant/utils.py
+++ backend/restaurant/utils.py
@@ -58,11 +58,10 @@ def send_approval_email(names, receiver, profile_title, profile_type):
         names = ["Restaurant Owner"]
     content = '<p>Hello ' + ', '.join(names) + ',</p>'
     if profile_type == 'restaurant':
-        content += '<p>Your restaurant profile for <i>' + profile_title + \
-            '</i> has been approved and is published to the live site.</p>'
+        content += '<p>Your restaurant profile for <i>'
     else:
-        content += '<p>Your food profile for <i>' + profile_title + \
-            '</i> has been approved and is published to the live site.</p>'
+        content += '<p>Your food profile for <i>'
+    content += profile_title + '</i> has been approved and is published to the live site.</p>'
     content += '<p>Thanks,</p>'
     content += '<p>Your Find Dining team</p>'
 
@@ -95,12 +94,12 @@ def send_reject_email(names, receiver, profile_title, profile_type):
         names = ["Restaurant Owner"]
     content = '<p>Hello ' + ', '.join(names) + ',</p>'
     if profile_type == 'restaurant':
-        content += '<p>Your restaurant profile for <i>' + profile_title
+        content += '<p>Your restaurant profile for <i>'
 
     else:
-        content += '<p>Your food profile for <i>' + profile_title
+        content += '<p>Your food profile for <i>'
 
-    content += '</i> has been rejected and the changes will not be applied to the live site.</p>' + \
+    content += profile_title + '</i> has been rejected and the changes will not be applied to the live site.</p>' + \
         '<p>Please review the comments from administrator and make appropriate changes.</p>'
     if profile_type == 'restaurant':
         content += '<p>Once you feel confident about your restaurant profile, feel free to submit it again for us to review!</p>'
@@ -139,11 +138,11 @@ def send_unpublish_email(names, receiver, profile_title, profile_type):
         names = ["Restaurant Owner"]
     content = '<p>Hello ' + ', '.join(names) + ',</p>'
     if profile_type == 'restaurant':
-        content += '<p>Your restaurant profile for <i>' + profile_titl
+        content += '<p>Your restaurant profile for <i>'
     else:
-        content += '<p>Your food profile for <i>' + profile_title
-    content += '</i> has been unpublished from the live site.</p>'
-    content += '<p>Please check the comments from administrator for the detail reason.</p>'
+        content += '<p>Your food profile for <i>'
+    content += profile_title + '</i> has been unpublished from the live site.</p>'
+    content += '<p>Please check the comments from administrator for the detailed reason.</p>'
     content += '<p>Note: this does not apply to the newest submission that has not been reviewed yet. If you have submitted a newer profile please wait for it to be reviewed.</p>'
     content += '<p>Thanks,</p>'
     content += '<p>Your Find Dining team</p>'
