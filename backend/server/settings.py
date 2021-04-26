a8071ae0289dfba4e00ec7a817421f9968c97e35
mouisaac <mouisaac88801@gmail.com>
2021-04-26 12:03:57 -0400

    added missing user status check; adjusted token expiry time;

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -287,8 +287,8 @@ SIMPLE_JWT = {
     # for testing
     # 'ACCESS_TOKEN_LIFETIME': timedelta(seconds=10),
     # 'REFRESH_TOKEN_LIFETIME': timedelta(seconds=60),
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hour=6),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=3),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': True,
