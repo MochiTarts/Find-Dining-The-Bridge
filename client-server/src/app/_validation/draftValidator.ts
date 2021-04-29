a0a13e8486ca98b7abdd61909254f1061fa505c8
mouisaac <mouisaac88801@gmail.com>
2021-04-29 17:26:52 -0400

    updated validation message for restaurant profile;

--- client-server/src/app/_validation/draftValidator.ts
+++ client-server/src/app/_validation/draftValidator.ts
@@ -55,7 +55,7 @@ export class draftValidator extends formValidator {
 
   errorStrings = {
     'name': 'Restaurant Name is required, and must be less than 255 characters.',
-    'years': 'Invalid Years in Business, ensure it is a non-negative number.',
+    'years': 'Years in Business must be a non-negative number.  Enter 1 if in business less than one year.',
     'address': 'Invalid Address - ensure the address is in Scarborough.',
     'streetAddress2': 'Invalid Address, must be less than 100 characters.',
     'streetAddress3': 'Invalid Address, must be less than 100 characters.',
